73322b4efa1902169bde11619e41e90349cc843c
NiranjanPowar <117906202+NiranjanPowar@users.noreply.github.com>
2023-04-24 19:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let arrOfKeys = Object.keys(sampleObject); //["red", "green", "white"];
+	return arrOfKeys.includes(key); 
 }
 
 // Do not change the code below
